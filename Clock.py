6f6d86d77620d21ce3c398f524246c1cfb1b228a
Huzaifa Imran <huzaifadsu@gmail.com>
2021-05-19 01:37:51 +0500

    formatting and parameter types

--- Clock.py
+++ Clock.py
@@ -2,10 +2,12 @@ import copy as cp
 import pygame
 import math
 
+
 class Clock():
     whiteColor = (255, 255, 255)
     patternSetting = True
-    def __init__(self, centerX, centerY, radius, baseSpeed):
+
+    def __init__(self, centerX: int, centerY: int, radius: int, baseSpeed):
         self.location1 = 0
         self.location2 = 180
         self.baseSpeed = baseSpeed
@@ -16,8 +18,8 @@ class Clock():
         self.allowUpdate = True
         self.speed1 = 0
         self.speed2 = 0
-        
-    def reinitiate(self, i:int, j:int, idirec, jdirec, allowUpdate):
+
+    def reinitiate(self, i: int, j: int, idirec, jdirec, allowUpdate):
         idirec = idirec*2 - 1
         jdirec = jdirec*2 - 1
         i = i % 628
@@ -28,28 +30,27 @@ class Clock():
         self.speed2 = self.baseSpeed * jdirec
         self.allowUpdate = allowUpdate
         self.patternSetting = True
-            
+
     def settingPattern(self):
         if self.patternSetting:
             self.patternSetting = False
             if self.location1 != self.location1Goal:
                 self.location1 = (self.location1 + self.speed1) % 628
                 self.patternSetting = True
-                
+
             if self.location2 != self.location2Goal:
                 self.location2 = (self.location2 + self.speed2) % 628
                 self.patternSetting = True
-                
-            
+
     def update(self):
         if self.allowUpdate:
             self.location1 = (self.location1 + self.speed1) % 628
             self.location2 = (self.location2 + self.speed2) % 628
-        
+
     def display(self, screen):
-        pygame.draw.circle(screen, self.whiteColor, (self.centerX, self.centerY), self.radius, 1)
-        pygame.draw.line(screen, self.whiteColor, (self.centerX, self.centerY), (self.centerX + math.cos(self.location1/100.0) * (self.radius-5), self.centerY + math.sin(self.location1/100.0) * (self.radius-5)), 4)
-        pygame.draw.line(screen, self.whiteColor, (self.centerX, self.centerY), (self.centerX + math.cos(self.location2/100.0) * (self.radius-5), self.centerY + math.sin(self.location2/100.0) * (self.radius-5)), 4)
-        
-        
-        
+        pygame.draw.circle(screen, self.whiteColor,
+                           (self.centerX, self.centerY), self.radius, 1)
+        pygame.draw.line(screen, self.whiteColor, (self.centerX, self.centerY), (self.centerX + math.cos(
+            self.location1/100.0) * (self.radius-5), self.centerY + math.sin(self.location1/100.0) * (self.radius-5)), 4)
+        pygame.draw.line(screen, self.whiteColor, (self.centerX, self.centerY), (self.centerX + math.cos(
+            self.location2/100.0) * (self.radius-5), self.centerY + math.sin(self.location2/100.0) * (self.radius-5)), 4)
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ patternTime = 13*framerate
 clockTime = 8*framerate
 counter = 0
 
+
 def setup():
     global clock, rowSize, screen
     pygame.init()
@@ -27,9 +28,11 @@ def setup():
     for i in range(rowSize):
         rowClock = []
         for j in range(colSize):
-            rowClock.append(Clock((j*2+1) * radius, (i*2+1) * radius, radius, 1))
+            rowClock.append(
+                Clock((j*2+1) * radius, (i*2+1) * radius, radius, 1))
         clock.append(rowClock)
-       
+
+
 def main():
     global counter
     timer = pygame.time.Clock()
@@ -46,7 +49,7 @@ def main():
             currentMinute = time.localtime().tm_min
             setPattern(4)
             counter = 0
-            
+
         for i in range(4):
             if counter == clockTime + patternTime*i:
                 setPattern(i)
@@ -58,6 +61,7 @@ def main():
             counter = clockTime
         timer.tick(framerate)
         pygame.display.update()
+
 
 def draw():
     screen.fill(blackColor)
@@ -76,29 +80,32 @@ def draw():
         for j in i:
             j.display(screen)
 
-            
+
 def setPattern(pattern):
     Clock.patternSetting = True
     if pattern == 0:
         distance = 10
         for i in range(len(clock)):
             for j in range(len(clock[i])):
-                clock[i][j].reinitiate(j*distance + BOT, j*distance + TOP, 0, 0, True)
-    
+                clock[i][j].reinitiate(
+                    j*distance + BOT, j*distance + TOP, 0, 0, True)
+
     if pattern == 1:
         for i in range(len(clock)):
-            baseAngle = (i%2) * LEFT
+            baseAngle = (i % 2) * LEFT
             for j in range(len(clock[i])):
                 k = (j+i) % 2
-                clock[i][j].reinitiate(k*BOT + baseAngle, (k+1)*BOT + baseAngle, 1, 0, True)
-                
+                clock[i][j].reinitiate(
+                    k*BOT + baseAngle, (k+1)*BOT + baseAngle, 1, 0, True)
+
     elif pattern == 2:
         for i in range(len(clock)):
-            baseAngle = (i%2) * LEFT
+            baseAngle = (i % 2) * LEFT
             for j in range(len(clock[i])):
                 k = (j+i) % 2
-                clock[i][j].reinitiate(k*BOT + baseAngle, (k+1)*BOT + baseAngle, (i+j)%2, (i+j)%2, True)
-                
+                clock[i][j].reinitiate(
+                    k*BOT + baseAngle, (k+1)*BOT + baseAngle, (i+j) % 2, (i+j) % 2, True)
+
     elif pattern == 3:
         distance = 10
         for i in range(len(clock)):
@@ -108,15 +115,15 @@ def setPattern(pattern):
         for i in range(len(clock)):
             for j in range(len(clock[i])):
                 clock[i][j].reinitiate(135, 135, 1, 0, False)
-                
+
         topDistance = -((rowSize-6) // -2)
         leftDistance = -((colSize-13) // -2)
         digitMaker[time.localtime().tm_hour // 10](topDistance, leftDistance)
         digitMaker[time.localtime().tm_hour % 10](topDistance, leftDistance+3)
         digitMaker[time.localtime().tm_min // 10](topDistance, leftDistance+7)
         digitMaker[time.localtime().tm_min % 10](topDistance, leftDistance+10)
-        
-        
+
+
 def createZero(row, col):
     for i in range(row, row+5):
         for j in range(col, col+3):
@@ -129,7 +136,8 @@ def createZero(row, col):
     clock[row+4][col+1].reinitiate(TOP, TOP, 1, 0,  False)
     clock[row][col+1].reinitiate(RIGHT, LEFT, 1, 0,  False)
     clock[row+5][col+1].reinitiate(RIGHT, LEFT, 1, 0,  False)
-    
+
+
 def createOne(row, col):
     clock[row][col+1].reinitiate(RIGHT, BOT, 1, 0,  False)
     clock[row+1][col+1].reinitiate(BOT, TOP, 1, 0,  False)
@@ -143,7 +151,8 @@ def createOne(row, col):
     clock[row+3][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+4][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+5][col+2].reinitiate(LEFT, TOP, 1, 0,  False)
-    
+
+
 def createTwo(row, col):
     clock[row][col].reinitiate(RIGHT, BOT, 1, 0,  False)
     clock[row+1][col].reinitiate(TOP, RIGHT, 1, 0,  False)
@@ -163,7 +172,8 @@ def createTwo(row, col):
     clock[row+3][col+2].reinitiate(LEFT, TOP, 1, 0,  False)
     clock[row+4][col+2].reinitiate(BOT, LEFT, 1, 0,  False)
     clock[row+5][col+2].reinitiate(LEFT, TOP, 1, 0,  False)
-    
+
+
 def createThree(row, col):
     clock[row][col].reinitiate(RIGHT, BOT, 1, 0,  False)
     clock[row+1][col].reinitiate(TOP, RIGHT, 1, 0,  False)
@@ -183,7 +193,8 @@ def createThree(row, col):
     clock[row+3][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+4][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+5][col+2].reinitiate(LEFT, TOP, 1, 0,  False)
-    
+
+
 def createFour(row, col):
     clock[row][col].reinitiate(RIGHT, BOT, 1, 0,  False)
     clock[row+1][col].reinitiate(BOT, TOP, 1, 0,  False)
@@ -201,7 +212,8 @@ def createFour(row, col):
     clock[row+3][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+4][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+5][col+2].reinitiate(LEFT, TOP, 1, 0,  False)
-    
+
+
 def createFive(row, col):
     clock[row][col].reinitiate(RIGHT, BOT, 1, 0,  False)
     clock[row+1][col].reinitiate(BOT, TOP, 1, 0,  False)
@@ -221,7 +233,8 @@ def createFive(row, col):
     clock[row+3][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+4][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+5][col+2].reinitiate(LEFT, TOP, 1, 0,  False)
-    
+
+
 def createSix(row, col):
     clock[row][col].reinitiate(RIGHT, BOT, 1, 0,  False)
     clock[row+1][col].reinitiate(BOT, TOP, 1, 0,  False)
@@ -241,7 +254,8 @@ def createSix(row, col):
     clock[row+3][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+4][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+5][col+2].reinitiate(LEFT, TOP, 1, 0,  False)
-    
+
+
 def createSeven(row, col):
     clock[row][col].reinitiate(RIGHT, BOT, 1, 0,  False)
     clock[row+1][col].reinitiate(TOP, RIGHT, 1, 0,  False)
@@ -257,7 +271,8 @@ def createSeven(row, col):
     clock[row][col+2].reinitiate(BOT, LEFT, 1, 0,  False)
     clock[row+1][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+2][col+2].reinitiate(235, TOP, 1, 0,  False)
-    
+
+
 def createEight(row, col):
     for i in range(row, row+5):
         for j in range(col, col+3):
@@ -272,7 +287,8 @@ def createEight(row, col):
     clock[row+4][col+1].reinitiate(453, 488, 1, 0,  False)
     clock[row][col+1].reinitiate(RIGHT, LEFT, 1, 0,  False)
     clock[row+5][col+1].reinitiate(RIGHT, LEFT, 1, 0,  False)
-    
+
+
 def createNine(row, col):
     clock[row][col].reinitiate(RIGHT, BOT, 1, 0,  False)
     clock[row+1][col].reinitiate(BOT, TOP, 1, 0,  False)
@@ -292,8 +308,10 @@ def createNine(row, col):
     clock[row+3][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+4][col+2].reinitiate(BOT, TOP, 1, 0,  False)
     clock[row+5][col+2].reinitiate(LEFT, TOP, 1, 0,  False)
-    
-digitMaker = [createZero, createOne, createTwo, createThree, createFour, createFive, createSix, createSeven, createEight, createNine]
+
+
+digitMaker = [createZero, createOne, createTwo, createThree, createFour,
+              createFive, createSix, createSeven, createEight, createNine]
 
 if __name__ == '__main__':
     main()
